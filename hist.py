afa5c789926bd01057be77422cc6a47954f70028
mhmmdpolatt <139051546+mhmmdpolatt@users.noreply.github.com>
2023-11-12 23:41:12 +0300

    src

--- hist.py
+++ hist.py
@@ -1,29 +1,36 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Örnek olarak bir gri tonlu resmi temsil eden bir matris
-resim = cv2.imread("image.jpg", 0)
+resim = cv2.imread("image.jpg")
 
-# Histogramı saklamak için boş bir sözlük oluştur
+B = resim[:,:,0]
+G = resim[:,:,1]
+R = resim[:,:,2]
+
+resim = 0.2989 * R + 0.5870 * G + 0.1140 * B
+
+
+
 histogram = {}
 
-# Her pikselin histogramını hesapla
+
 for satir in resim:
     for piksel in satir:
+        piksel = int(piksel)
         if piksel in histogram:
+            
             histogram[piksel] += 1
         else:
             histogram[piksel] = 1
 
-# Histogramı ekrana yazdır
+
 for renk_degeri, frekans in histogram.items():
     print(f"{renk_degeri}: {frekans}")
 
-# Histogram verilerini ayrıştır
+
 renk_degerleri = list(histogram.keys())
 frekanslar = list(histogram.values())
 
-# Histogramı görselleştir
 plt.bar(renk_degerleri, frekanslar, color='blue')
 plt.xlabel('Renk Değeri')
 plt.ylabel('Frekans')
